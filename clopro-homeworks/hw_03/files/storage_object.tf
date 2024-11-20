b94497ecb9b9f433b67b138eccf6f6bb78c5150b
reocoker85 <139924408+reocoker85@users.noreply.github.com>
2024-11-20 16:28:11 +0300

    Add files via upload

--- clopro-homeworks/hw_03/files/storage_object.tf
+++ clopro-homeworks/hw_03/files/storage_object.tf
@@ -1,11 +1,3 @@
-resource "random_string" "unique_id" {
-  length  = 8
-  upper   = false
-  lower   = true
-  numeric = true
-  special = false
-}
-
 resource "yandex_iam_service_account" "sa" {
   name = "sa"
 }
@@ -16,76 +8,67 @@ resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
   member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
 }
 
-#resource "yandex_resourcemanager_folder_iam_member" "encrypter" {
-#  folder_id = var.folder_id
-#  role      = "kms.keys.encrypterDecrypter"
-#  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
-#}
+resource "yandex_resourcemanager_folder_iam_member" "encrypter" {
+  folder_id = var.folder_id
+  role      = "kms.keys.encrypterDecrypter"
+  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
+}
 
 resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
   service_account_id = yandex_iam_service_account.sa.id
   description        = "static access key for object storage"
 }
 
-#resource "yandex_kms_symmetric_key" "key-a" {
-#  name              = "example-symetric-key"
-#  description       = "description for key"
-#  default_algorithm = "AES_128"
-#  rotation_period   = "168h" 
-#}
-
-#resource "yandex_cm_certificate" "example" {
-#  name    = "example"
-#  domains = ["bucket-${random_string.unique_id.result}.website.yandexcloud.net"]
-#
-#  managed {
-#    challenge_type = "HTTP"
-#  }
-#}
+resource "yandex_kms_symmetric_key" "key-a" {
+  name              = "example-symetric-key"
+  description       = "description for key"
+  default_algorithm = "AES_128"
+  rotation_period   = "168h" 
+}
 
 resource "yandex_storage_bucket" "test" {
   access_key            = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   secret_key            = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  bucket                = "bucket-${random_string.unique_id.result}"
+  bucket                = "reocoker.ru"
   max_size              = 1073741824
   acl                   = "public-read"
   website {
     index_document = "index.html"
     error_document = "error.html"
-    routing_rules  = <<EOF
-    [{
-        "Condition": {
-            "KeyPrefixEquals": "docs/"
-        },
-        "Redirect": {
-            "ReplaceKeyPrefixWith": "documents/"
-        }
-    }]
-    EOF
   }
-#  https  {
-#    certificate_id = yandex_cm_certificate.example.id
-#  }
-#  server_side_encryption_configuration {
-#    rule {
-#      apply_server_side_encryption_by_default {
-#        kms_master_key_id = yandex_kms_symmetric_key.key-a.id
-#        sse_algorithm     = "aws:kms"
-#      }
-#    }
-#  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = yandex_kms_symmetric_key.key-a.id
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
 
-resource "yandex_storage_object" "test-object" {
+resource "yandex_storage_object" "object-image" {
   access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  bucket     = "bucket-${random_string.unique_id.result}"
+  bucket     = "reocoker.ru"
   key        = "devops.jpg"
   source     = "./devops.jpg"
   acl        = "public-read"
   depends_on = [
                 yandex_storage_bucket.test,
-#                yandex_kms_symmetric_key.key-a
+                yandex_kms_symmetric_key.key-a
+               ]
+}
+
+resource "yandex_storage_object" "object-html" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = "reocoker.ru"
+  key        = "index.html"
+  source     = "./index.html"
+  acl        = "public-read"
+  depends_on = [
+                yandex_storage_bucket.test,
+                yandex_kms_symmetric_key.key-a
                ]
 }
 
