1ba2b0272e8736bbead5f29cc35131cb5df8f39e
reocoker85 <139924408+reocoker85@users.noreply.github.com>
2024-11-25 17:22:35 +0300

    Add files via upload

---
+++ clopro-homeworks/hw_04/files/task_kub/main.tf
@@ -0,0 +1,194 @@
+resource "yandex_kubernetes_cluster" "k8s-regional" {
+  name = "k8s-regional"
+  network_id = yandex_vpc_network.my_vpc.id
+  master {
+    master_location {
+      zone      = yandex_vpc_subnet.mysubnet-a.zone
+      subnet_id = yandex_vpc_subnet.mysubnet-a.id
+    }
+    master_location {
+      zone      = yandex_vpc_subnet.mysubnet-b.zone
+      subnet_id = yandex_vpc_subnet.mysubnet-b.id
+    }
+    master_location {
+      zone      = yandex_vpc_subnet.mysubnet-d.zone
+      subnet_id = yandex_vpc_subnet.mysubnet-d.id
+    }
+    security_group_ids = [yandex_vpc_security_group.regional-k8s-sg.id]
+  }
+  service_account_id      = yandex_iam_service_account.my-regional-account.id
+  node_service_account_id = yandex_iam_service_account.my-regional-account.id
+  depends_on = [
+    yandex_resourcemanager_folder_iam_member.k8s-clusters-agent,
+    yandex_resourcemanager_folder_iam_member.vpc-public-admin,
+    yandex_resourcemanager_folder_iam_member.images-puller,
+    yandex_resourcemanager_folder_iam_member.encrypterDecrypter
+  ]
+  kms_provider {
+    key_id = yandex_kms_symmetric_key.kms-key.id
+  }
+}
+
+resource "yandex_kubernetes_node_group" "my_node_group" {
+  cluster_id  = yandex_kubernetes_cluster.k8s-regional.id
+  name        = "name"
+  description = "description"
+  version     = "1.27"
+
+ # labels = {
+ #   "key" = "value"
+ # }
+
+  instance_template {
+    platform_id = "standard-v1"
+
+    network_interface {
+      nat        = false
+      subnet_ids = [yandex_vpc_subnet.mysubnet-b.id]
+    }
+
+    resources {
+      memory = 2
+      cores  = 2
+    }
+
+    boot_disk {
+      type = "network-hdd"
+      size = 30
+    }
+
+    scheduling_policy {
+      preemptible = true
+    }
+
+    container_runtime {
+      type = "containerd"
+    }
+  }
+
+  scale_policy {
+    auto_scale {
+      min     = 3
+      max     = 6
+      initial = 3
+    }
+  }
+
+  allocation_policy {
+    location {
+      zone = "ru-central1-b" 
+    }
+  }
+}
+
+resource "yandex_vpc_network" "my_vpc" {
+  name = var.vpc_name
+}
+
+resource "yandex_vpc_subnet" "mysubnet-a" {
+  name = "mysubnet-a"
+  v4_cidr_blocks = ["10.5.0.0/16"]
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.my_vpc.id
+}
+
+resource "yandex_vpc_subnet" "mysubnet-b" {
+  name = "mysubnet-b"
+  v4_cidr_blocks = ["10.6.0.0/16"]
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.my_vpc.id
+}
+
+resource "yandex_vpc_subnet" "mysubnet-d" {
+  name = "mysubnet-d"
+  v4_cidr_blocks = ["10.7.0.0/16"]
+  zone           = "ru-central1-d"
+  network_id     = yandex_vpc_network.my_vpc.id
+}
+
+resource "yandex_iam_service_account" "my-regional-account" {
+  name        = "regional-k8s-account"
+  description = "K8S regional service account"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "k8s-clusters-agent" {
+  # Сервисному аккаунту назначается роль "k8s.clusters.agent".
+  folder_id = var.folder_id
+  role      = "k8s.clusters.agent"
+  member    = "serviceAccount:${yandex_iam_service_account.my-regional-account.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "vpc-public-admin" {
+  # Сервисному аккаунту назначается роль "vpc.publicAdmin".
+  folder_id = var.folder_id
+  role      = "vpc.publicAdmin"
+  member    = "serviceAccount:${yandex_iam_service_account.my-regional-account.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "images-puller" {
+  # Сервисному аккаунту назначается роль "container-registry.images.puller".
+  folder_id = var.folder_id
+  role      = "container-registry.images.puller"
+  member    = "serviceAccount:${yandex_iam_service_account.my-regional-account.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "encrypterDecrypter" {
+  # Сервисному аккаунту назначается роль "kms.keys.encrypterDecrypter".
+  folder_id = var.folder_id
+  role      = "kms.keys.encrypterDecrypter"
+  member    = "serviceAccount:${yandex_iam_service_account.my-regional-account.id}"
+}
+
+resource "yandex_kms_symmetric_key" "kms-key" {
+  # Ключ Yandex Key Management Service для шифрования важной информации, такой как пароли, OAuth-токены и SSH-ключи.
+  name              = "kms-key"
+  default_algorithm = "AES_128"
+  rotation_period   = "8760h" # 1 год.
+}
+
+resource "yandex_vpc_security_group" "regional-k8s-sg" {
+  name        = "regional-k8s-sg"
+  description = "Правила группы обеспечивают базовую работоспособность кластера Managed Service for Kubernetes. Примените ее к кластеру и группам узлов."
+  network_id  = yandex_vpc_network.my_vpc.id
+  ingress {
+    protocol          = "TCP"
+    description       = "Правило разрешает проверки доступности с диапазона адресов балансировщика нагрузки. Нужно для работы отказоустойчивого кластера Managed Service for Kubernetes и сервисов балансировщика."
+    predefined_target = "loadbalancer_healthchecks"
+    from_port         = 0
+    to_port           = 65535
+  }
+  ingress {
+    protocol          = "ANY"
+    description       = "Правило разрешает взаимодействие мастер-узел и узел-узел внутри группы безопасности."
+    predefined_target = "self_security_group"
+    from_port         = 0
+    to_port           = 65535
+  }
+  ingress {
+    protocol          = "ANY"
+    description       = "Правило разрешает взаимодействие под-под и сервис-сервис. Укажите подсети вашего кластера Managed Service for Kubernetes и сервисов."
+    v4_cidr_blocks    = concat(yandex_vpc_subnet.mysubnet-a.v4_cidr_blocks, yandex_vpc_subnet.mysubnet-b.v4_cidr_blocks, yandex_vpc_subnet.mysubnet-d.v4_cidr_blocks)
+    from_port         = 0
+    to_port           = 65535
+  }
+  ingress {
+    protocol          = "ICMP"
+    description       = "Правило разрешает отладочные ICMP-пакеты из внутренних подсетей."
+    v4_cidr_blocks    = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+  }
+  ingress {
+    protocol          = "TCP"
+    description       = "Правило разрешает входящий трафик из интернета на диапазон портов NodePort. Добавьте или измените порты на нужные вам."
+    v4_cidr_blocks    = ["0.0.0.0/0"]
+    from_port         = 30000
+    to_port           = 32767
+  }
+  egress {
+    protocol          = "ANY"
+    description       = "Правило разрешает весь исходящий трафик. Узлы могут связаться с Yandex Container Registry, Yandex Object Storage, Docker Hub и т. д."
+    v4_cidr_blocks    = ["0.0.0.0/0"]
+    from_port         = 0
+    to_port           = 65535
+  }
+}
+
---
+++ clopro-homeworks/hw_04/files/task_kub/variables.tf
@@ -0,0 +1,102 @@
+###cloud vars
+variable "token" {
+  type        = string
+  sensitive   = true
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "cloud_id" {
+  type        = string
+  sensitive   = true
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  sensitive   = true
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "subnet_name" {
+  type        = string
+  default     = "private"
+  description = "Name of public subnet"
+}
+
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["192.168.0.0/16"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "default_zone" {
+  type    = string
+  default = "ru-central1-a"
+}
+
+variable "zones" {
+  type    = list(string)
+  default = ["ru-central1-a", "ru-central1-b"]
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "my_vpc"
+  description = "VPC network&subnet name"
+}
+
+###cluster vars
+variable "cluster" {
+  type = object({
+    mysql = object({
+      name         = string
+      environment  = string
+      version      = string
+      del_protec   = bool
+      resource_id  = string
+      disk_type_id = string
+      disk_size    = number
+      public_ip    = bool
+      type         = string
+      back_h       = number
+      back_m       = number
+    })
+  })
+  default = {
+    mysql = {
+      name         = "cluster_mysql"
+      environment  = "PRESTABLE"
+      version      = "8.0"
+      del_protec   = false
+      resource_id  = "b1.medium"
+      disk_type_id = "network-hdd"
+      disk_size    = 20
+      public_ip    = false
+      type         = "ANYTIME"
+      back_h       = 23
+      back_m       = 59
+    }
+  }
+}
+
+###bd vars
+variable "bd" {
+  type = object({
+    netology = object({
+      name      = string
+      user_name = string
+      password  = string
+      roles     = string
+    })
+  })
+  default = {
+    netology = {
+      name      = "netology_db"
+      user_name = "reo"
+      password  = "qwer1234"
+      roles     = "ALL"
+    }
+  }
+}
+
+
